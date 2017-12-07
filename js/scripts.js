701dc2e92c8dbf1591079a0cc86cf33e2a21bfac
Sebastian Bauer <sebastian.bauer@wp.pl>
2017-12-08 00:10:04 +0100

    Zoptymalizowano kod (z 4 eventListener-ow do kazdego buttona zrobiono tylko jeden dla calego formularza)

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
-'use sctrict';
+'use strict';
 
-function dzialanie() {
+
+function dzialanie(operation) {
     this.collectionInput = document.querySelectorAll('input');
     this.num1 = parseFloat(this.collectionInput[0].value);
     this.num2 = parseFloat(this.collectionInput[1].value);
-
+    
     this.calculator = function(operation){
         
         switch(operation) {
@@ -28,38 +29,15 @@ function dzialanie() {
         }
  
     return this.wyn;
-        
     }
+    
 }
 
-var suma = document.getElementById('suma');
-suma.addEventListener('click', function(e){
+var calc = document.getElementById('calc');
+calc.addEventListener('submit', function(e){
     e.preventDefault();
     var wynik = document.getElementById('wynik');
     var calc = new dzialanie();
-    wynik.innerHTML = calc.calculator('+');
+    wynik.innerHTML = calc.calculator(document.activeElement.value);
 });
 
-var odejmowanie = document.getElementById('odejmowanie');
-odejmowanie.addEventListener('click', function(e){
-    e.preventDefault();
-    var wynik = document.getElementById('wynik');
-    var calc = new dzialanie();
-    wynik.innerHTML = calc.calculator('-');
-});
-
-var mnozenie = document.getElementById('mnozenie');
-mnozenie.addEventListener('click', function(e){
-    e.preventDefault();
-    var wynik = document.getElementById('wynik');
-    var calc = new dzialanie();
-    wynik.innerHTML = calc.calculator('*');
-});
-
-var dzielenie = document.getElementById('dzielenie');
-dzielenie.addEventListener('click', function(e){
-    e.preventDefault();
-    var wynik = document.getElementById('wynik');
-    var calc = new dzialanie();
-    wynik.innerHTML = calc.calculator('/');
-});
